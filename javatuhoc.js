a29e1e9dbefdbdb63d222ff2632a1edb1d2e2799
nguyentruong032 <thesucceeder032045@gmail.com>
2024-01-13 21:25:57 +0700

    init project

---
+++ javatuhoc.js
@@ -0,0 +1,12 @@
+// // hiển thị thông tin muốn hiển thị trên trình duyệt
+// // alert("xin chào");
+// // alert("tôi là tùng con ");
+// // alert("phòng 207");
+// console.log("đây là thử nghiệm");
+// // hiển thị ô nhập liệu
+// prompt("mời bạn nhập tên");
+// // console.log("let Number = 100");
+
+let ten = "truong";
+let diem = 100;
+console.log(" ten cua toi la " + ten + " " + diem);
